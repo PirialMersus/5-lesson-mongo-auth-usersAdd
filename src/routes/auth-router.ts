2d031ed61c892a9a40641f10d330ca8e84008933
Gennadii <atlantagenafes@gmail.com>
2022-10-08 13:44:19 +0300

    version 0.15

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -9,10 +9,10 @@ export const authRouter = Router({})
 authRouter
     .post('/login',
         // authMiddleware,
-        body('login').trim().not().isEmpty().withMessage('enter input value in name field'),
-        body('password').trim().not().isEmpty().withMessage('enter input value in password field'),
-        body('login').isLength({max: 10, min: 3}).withMessage('login: maxLength: 10 minLength: 3'),
-        body('password').isLength({min: 6, max: 20}).withMessage('password: min: 6, max: 20'),
+        // body('login').trim().not().isEmpty().withMessage('enter input value in name field'),
+        // body('password').trim().not().isEmpty().withMessage('enter input value in password field'),
+        // body('login').isLength({max: 10, min: 3}).withMessage('login: maxLength: 10 minLength: 3'),
+        // body('password').isLength({min: 6, max: 20}).withMessage('password: min: 6, max: 20'),
 
         inputValidatorMiddleware,
         async (req: Request, res: Response) => {
